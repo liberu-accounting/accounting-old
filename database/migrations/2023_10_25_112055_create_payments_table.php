2176d34c56d6a01ebaaee2028cba77e6e9c742cb
Alain-Kay <kazadikayumba@gmail.com>
2023-10-25 10:23:46 +0200

    added payment migration and model

---
+++ database/migrations/2023_10_25_112055_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->integer('payment_id', true);
+            $table->integer('invoice_id');
+            $table->decimal('payment_amount', 10, 2);
+            $table->date('payment_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
